3a03fce54b01c02c892e53f1a1027f9d12f8fe2e
r-dziewaltowski <135457365+r-dziewaltowski@users.noreply.github.com>
2024-09-24 01:08:39 +0100

    More tests

---
+++ tests/CityPassGuide.UnitTests/Core/CityCardAggregate/CityTests.cs
@@ -0,0 +1,45 @@
+﻿using CityPassGuide.Core.CityCardAggregate;
+using Xunit;
+
+namespace CityPassGuide.UnitTests.Core.CityCardAggregate;
+public class CityTests
+{
+  private const string Name = "Edinburgh";
+  private const int CountryId = 1;
+  private const int DailyTransportCost = 15;
+
+  [Fact]
+  public void Constructor_ShouldSetProperties()
+  {
+    // Act
+    var country = new City(Name, CountryId, DailyTransportCost);
+
+    // Assert
+    Assert.Equal(Name, country.Name);
+    Assert.Equal(CountryId, country.CountryId);
+    Assert.Equal(DailyTransportCost, country.DailyTransportCost);
+  }
+
+  [Fact]
+  public void Constructor_ShouldThrow_WhenNameEmpty()
+  {
+    // Act + Assert
+    Assert.Throws<ArgumentException>(() => new City("", CountryId, DailyTransportCost));
+  }
+
+  [Theory]
+  [InlineData(-1)]
+  [InlineData(0)]
+  public void Constructor_ShouldThrow_WhenCountryIdIsIncorrect(int countryId)
+  {
+    // Act + Assert
+    Assert.Throws<ArgumentException>(() => new City(Name, countryId, DailyTransportCost));
+  }
+
+  [Fact]
+  public void Constructor_ShouldThrow_WhenPriceIsNegative()
+  {
+    // Act + Assert
+    Assert.Throws<ArgumentException>(() => new City(Name, CountryId, -1));
+  }
+}
---
+++ tests/CityPassGuide.UnitTests/Core/CityCardAggregate/CountryTests.cs
@@ -0,0 +1,25 @@
+﻿using CityPassGuide.Core.CityCardAggregate;
+using Xunit;
+
+namespace CityPassGuide.UnitTests.Core.CityCardAggregate;
+public class CountryTests
+{
+  private const string Name = "UK";
+
+  [Fact]
+  public void Constructor_ShouldSetProperties()
+  {
+    // Act
+    var country = new Country(Name);
+
+    // Assert
+    Assert.Equal(Name, country.Name);
+  }
+
+  [Fact]
+  public void Constructor_ShouldThrow_WhenNameEmpty()
+  {
+    // Act + Assert
+    Assert.Throws<ArgumentException>(() => new Country(""));
+  }
+}
