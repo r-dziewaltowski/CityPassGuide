5fe68d7d8380c7a717ce40c93599bd825d303a41
r-dziewaltowski <135457365+r-dziewaltowski@users.noreply.github.com>
2024-11-15 17:58:55 +0000

    URLs lowercase

--- src/CityPassGuide.Web/Countries/GetCountryByIdEndpoint.GetCountryByIdRequest.cs
+++ src/CityPassGuide.Web/Countries/GetCountryByIdEndpoint.GetCountryByIdRequest.cs
@@ -2,7 +2,7 @@
 
 public class GetCountryByIdRequest
 {
-  public const string Route = $"/Countries/{{{CountryIdParamName}}}";
+  public const string Route = $"/countries/{{{CountryIdParamName}}}";
   public const string CountryIdParamName = nameof(CountryId);
 
   public int CountryId { get; set; }
